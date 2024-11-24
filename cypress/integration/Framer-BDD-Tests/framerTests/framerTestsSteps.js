5f6ebdce540cf018b4a0aec254705f623366be85
MG0007 <103684005+MG0007@users.noreply.github.com>
2024-11-24 12:55:42 +0200

    Added a second test

--- cypress/integration/Framer-BDD-Tests/framerTests/framerTestsSteps.js
+++ cypress/integration/Framer-BDD-Tests/framerTests/framerTestsSteps.js
@@ -44,3 +44,21 @@ When('I scroll to Many types of components to customize in the components page',
     cy.get('div.framer-1lz646s h2 span.framer-text').scrollTo('right')
     cy.get('div.framer-1lz646s h2 span.framer-text').should('be.visible').and('have.text', 'Many types of components to customize');
 })
+
+Then('I see the visit button is displayed on each card in the Many types of components to customize section', ()=>{
+    const columns = cy.get('div.framer-1ytqfhu').children()
+
+    columns.forEach((column, index) =>{
+        if(index !== 0 && index !== columns.length - 1){
+            const cards = column.get('[name="Card"]').children()
+            cards.forEach((card) =>{
+                card.get('[name="Card"] div[data-border="true"][name="Button"]').contains('Visit')
+            })
+
+            const cardsSmall = column.get('[name="Card Small"]').children()
+            cardsSmall.forEach((card)=>{
+                card.get('[name="Card Small"] div[data-border="true"][name="Button"]').contains('Visit')
+            })
+        }
+    })
+})
