720b57419260634fb9935fe404020a88ac383d03
MG0007 <103684005+MG0007@users.noreply.github.com>
2024-11-25 04:21:10 +0200

    Imported page into step definition file

---
+++ features/step_definitions/framerTestsSteps.js
@@ -0,0 +1,29 @@
+import { Given, When, Then, Before } from  "@cucumber/cucumber";
+import { expect } from  "@playwright/test";
+import { chromium } from "@playwright/test";
+
+Before(async () =>{
+    global.browser = await chromium.launch();
+  
+    global.page = await global.browser.newPage();
+})
+
+Given('the Framer site is loaded', async () => {
+    await page.goto('https://mind-wend-913065.framer.app/');
+  });
+
+  When('I click on Pricing button', async () => {
+    await page.locator('div.framer-1g3amks-container div[data-framer-generated="true"] span').click();
+  });
+
+  When('I see the Pricing page', async () => {
+    await page.locator('div.framer-1xy1tag h1 span').toContainText('Pricing');
+  });
+
+  Then('I see the products currency symbol is displayed as $ in Pricing page', async () => {
+    const tiles = await page.locator('div.pricing-container').locator('div');
+    tiles.forEach((tile) => {
+       const text = page.locator(tile + ' h2 span').textContent();
+       expect(text[0]).toHaveTExt('$');
+    });
+  });
